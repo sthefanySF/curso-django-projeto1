4c8a1960a0f233a276a4fc00b4761c9f38c37e24
SthefanySF <sthefanysantos0815@gmail.com>
2023-06-07 15:21:49 -0500

    inserindo dados fakes

--- receita/views.py
+++ receita/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from Utils.receitas.factory import make_recipe
 
 def home(request):
     return render(request,'receita/pages/home.html', context={
-        'name' : 'sthefany',
+        'receitas' : [make_recipe() for _ in range(10)],
     })
 
 def receita(request, id):
     return render(request,'receita/pages/receita-view.html', context={
-        'name' : 'sthefany',
+        'receita' : make_recipe(),
     })

7dcb7dd3568cfd38294c101e7d182d74ede24477
SthefanySF <sthefanysantos0815@gmail.com>
2023-06-13 17:38:36 -0500

    parte do admin

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings 
+from django.conf.urls.static import static #configurar os arquivos staticos nas urls
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,3 +30,5 @@ urlpatterns = [
 
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- receita/models.py
+++ receita/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User #usuario do django/tabela da base de dados
 
-# Create your models here.
+
 class Category(models.Model): #tabela de categoria de receitas
     name = models.CharField(max_length=65)
+
+    def __str__(self): #metodo magico para salvar o nome da categonia de acordo com o que foi escrito 
+        return self.name
+
 
 class Recipe(models.Model): #tabela de receitas
     title = models.CharField(max_length=65)
@@ -18,7 +22,7 @@ class Recipe(models.Model): #tabela de receitas
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='receitas/covers/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='receita/covers/%Y/%m/%d/')
     #relação de tabelas
 
     category = models.ForeignKey(
@@ -30,5 +34,7 @@ class Recipe(models.Model): #tabela de receitas
 
     
 
+    def __str__(self):
+        return self.title #para que o titulo da receita que a gente colocou apareça
 
     
